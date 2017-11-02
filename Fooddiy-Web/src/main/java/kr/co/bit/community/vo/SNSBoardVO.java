edbfd611445398bcbac8ef88af3ebc3b62353ef7
bit-user <bit-user@DESKTOP-C1QR4BV>
2017-11-02 17:03:41 +0900

    VO

--- Fooddiy-Web/src/main/java/kr/co/bit/community/vo/SNSBoardVO.java
+++ Fooddiy-Web/src/main/java/kr/co/bit/community/vo/SNSBoardVO.java
@@ -1,5 +1,48 @@
 package kr.co.bit.community.vo;
 
 public class SNSBoardVO {
+	
+	private int cNo;
+	private int no;
+	private String writer;
+	private String content;
+	private String regDate;
+	
+	public int getcNo() {
+		return cNo;
+	}
+	public void setcNo(int cNo) {
+		this.cNo = cNo;
+	}
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+	@Override
+	public String toString() {
+		return "SNSBoardVO [cNo=" + cNo + ", no=" + no + ", writer=" + writer + ", content=" + content + ", regDate="
+				+ regDate + "]";
+	}
 
+	
 }
